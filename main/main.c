669faaa68c000983b709aaef9115aca949ad3ac9
Kristopher Lancaster <KR.Lancaster@hotmail.com>
2026-05-11 02:57:57 -0600

    updated sdconfig and partitions

--- main/main.c
+++ main/main.c
@@ -2,7 +2,12 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+//
+#include "lv_demos.h"
+//
 #include "bsp/esp-bsp.h"
+#define LOG_MEM_INFO (0)
 
 void app_main(void)
 {
@@ -11,6 +16,10 @@ void app_main(void)
     bsp_display_start();
     /* Set display brightness to 100% */
     bsp_display_backlight_on();
+
+    bsp_display_lock(0);
+    lv_demo_widgets(); /* A widgets example */
+    bsp_display_unlock();
 
     while (1)
     {
