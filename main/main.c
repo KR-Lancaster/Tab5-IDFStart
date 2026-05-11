daf941ffc51497fdf1bc0abd13ce10c4904da305
Kristopher Lancaster <KR.Lancaster@hotmail.com>
2026-05-11 01:42:51 -0600

    ESP IDF 5.5.4 Tab5 Starting Code With FreeRTOS

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    ESP_LOGI("MAIN", "Main Started");
+
+    while (1)
+    {
+        ESP_LOGI("MAIN", "Running...");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
