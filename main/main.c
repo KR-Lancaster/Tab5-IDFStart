3043f2d991f660a618ac75472b3f442fd4d4bf62
Kristopher Lancaster <KR.Lancaster@hotmail.com>
2026-05-11 02:21:45 -0600

    BSP Package Working For LCD Init

--- main/main.c
+++ main/main.c
@@ -2,10 +2,15 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "bsp/esp-bsp.h"
 
 void app_main(void)
 {
     ESP_LOGI("MAIN", "Main Started");
+    /* Initialize display and LVGL */
+    bsp_display_start();
+    /* Set display brightness to 100% */
+    bsp_display_backlight_on();
 
     while (1)
     {
